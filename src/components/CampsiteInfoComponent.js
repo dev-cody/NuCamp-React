73109856f978a8ba667053a60fbec4b135d0c647
cody.koscielski <cody.koscielski@outlook.com>
2020-04-09 23:01:03 -0400

    Json server practice

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -4,13 +4,14 @@ import { Control, LocalForm, Errors, } from "react-redux-form"
 import { Link } from 'react-router-dom';
 import { Component } from "react";
 import { Loading } from "./LoadingComponent";
+import { baseUrl } from '../shared/baseUrl';
 
     //Render Campsites
 function RenderCampsite({campsite,}) {
     return (
         <div className="col-md-5 m-1">
             <Card>
-                <CardImg top src={campsite.image} alt={campsite.name} />
+                <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
                 <CardBody>
                     <CardText>{campsite.description}</CardText>
                 </CardBody>
